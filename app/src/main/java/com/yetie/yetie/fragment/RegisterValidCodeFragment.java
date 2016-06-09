777faf331f48fc9125391a17aca468e37f562a8d
Jean-Philippe Vignolo <jpvignolo@free.fr>
2016-06-09 04:09:41 +0200

    WIP

--- app/src/main/java/com/yetie/yetie/fragment/RegisterValidCodeFragment.java
+++ app/src/main/java/com/yetie/yetie/fragment/RegisterValidCodeFragment.java
@@ -2,36 +2,41 @@ package com.yetie.yetie.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.jipouille.usefullviews.views.EditTextPlusPhoneNumber;
+import com.jipouille.usefullviews.views.TextViewPlus;
 import com.yetie.yetie.R;
 import com.yetie.yetie.listeners.LoginFragmentListener;
+
+import me.philio.pinentry.PinEntryView;
 
 
 /**
  * Created by jp on 06/06/16.
  */
-public class RegisterValidCodeFragment extends Fragment implements MaterialDialog.SingleButtonCallback {
+public class RegisterValidCodeFragment extends Fragment {
     private static final String TAG = "RegisterPhoneFragment";
     LoginFragmentListener mCallback;
-    EditTextPlusPhoneNumber number;
+    PinEntryView code;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String fulldesc = null;
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_register_valid_code, container, false);
-
+        Bundle bundle = getArguments();
         ImageButton next = (ImageButton)v.findViewById(R.id.btn_next);
+        TextView description = (TextView) v.findViewById(R.id.desc);
+        if (bundle.getString("phonenumber") != null)
+            fulldesc = description.getText().toString()+" "+bundle.getString("phonenumber");
+        description.setText(fulldesc);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,10 +55,5 @@ public class RegisterValidCodeFragment extends Fragment implements MaterialDialo
             throw new ClassCastException(ctx.toString()
                     + " must implement SearchFragmentListener");
         }
-    }
-
-    @Override
-    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-        mCallback.onPhoneRegister(number.getText().toString());
     }
 }
